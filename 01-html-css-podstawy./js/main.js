d156adfc6061b05b62f9587db496193a0b932207
Tomasz <Tombranmak@gmail.com>
2022-11-06 09:13:35 +0100

    first commit

---
+++ 01-html-css-podstawy./js/main.js
@@ -0,0 +1,77 @@
+// alert to jest omenda ktora uruchamia okno dialogowe z tekstem w nawiasach
+// alert('Hello Ted')
+
+console.log('Jak Masz na imie?');
+
+console.log ('Jak Masz na imie?');
+
+// 1:  okna dialogowe
+
+// alert('siema eniu')
+
+
+
+
+// const to jest przpisanie do zmiennej 
+
+
+// const nazwaZmiennej - wartosci
+
+// konwencja nazywaniaZmiennej zmiennych: camelCase
+// do klas CSS uzywamy konwencji kebab-case
+
+// CamelCase mojeimietoTed
+// kebab case moje-imie-to-Ted
+
+const myName =prompt('Jak Masz na imie?');
+
+// consol.log(myName)
+
+// tworzenie zdan ze zmienych
+
+// 1 sposob - za pomoca znaka
+console .log('Hello' + myName);
+// 2. za pomoca tzw template string (ES6)
+
+console.log(`Hello ${myName}`)
+
+// Warunki
+
+// jedynym z typow zmiennych w js jest tzw boolean (prawda/falsz)
+
+// znaki >,>-,<,<-,--,---
+
+// console.log(10 > 10); false 
+// console.log(10 -- 10); ture
+// console.log(`Damian`  -- `Pawel`)// false
+// console.log(`Damian` -- `Damian`);// true
+
+// instrukcje warunkowe
+
+const age = prompt('Ile masz lat ?');
+
+if(age >=25) {
+ console.log('Mozesz wejsc do sali vip') ;
+}else if (age >- 18) {
+    console.log('mozesz wejsc do sali')
+}else{
+    console.log('Jestes za mlody');
+}
+
+
+
+// 1.Napisz Program ktory pobiera 2 liczby uzyj 2 razy funkcji front nastepnie napisz w konsoli ktora liczba jest wieksza jesli pierwsza liczba jest wiksza napisz liczba pierwasza liczba x jest wieksza od 2 y w przeciwynm wypadku napisz inna wiadomosc
+
+ const ege= prompt('ile lat ma marylarodowicz');
+
+ if(age >=51) {
+    console.log('Nie no troche wiecej mi sie wydaje ale blisko byles');
+ }else if (age >-31) {
+    console.log('w jakim ty swiecie zyjesz?')
+ }else{
+    console.log('Dowidzenia')
+ }
+// Napiz program ktory imituje gre w kamien pamiper nozyce popros napier o wybor pierwszego uzywtkownika popros potem o wybor 2 uzytkownika a nastepne napisz program ktory decyduje kto wygral
+
+
+
